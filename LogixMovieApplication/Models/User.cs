ccb8b3b9058fa61ee4c0bf8f4f62e749d8faff3e
linhvuquach <quachvulinhag1997@gmail.com>
2023-08-10 01:07:12 +0700

    [LMAS-09] feat: hash password

--- LogixMovieApplication/Models/User.cs
+++ LogixMovieApplication/Models/User.cs
@@ -8,5 +8,6 @@ namespace Logix_Movie_Application.Models
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
+        public string Salt { get; set; }
     }
 }
